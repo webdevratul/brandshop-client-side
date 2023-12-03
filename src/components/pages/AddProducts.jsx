5754740b7d6ecd472a559539450dd0223d5bc57d
webdevratul <mahibulislamratul@gmail.com>
2023-12-03 14:53:50 +0600

    make form resposnive and get formdata and post form data to server side for insert to database

--- src/components/pages/AddProducts.jsx
+++ src/components/pages/AddProducts.jsx
@@ -1,26 +1,66 @@
+import Swal from "sweetalert2";
 
 
 const AddProducts = () => {
+
+    const handleAddProduct = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const photo = form.photo.value;
+        const name = form.name.value;
+        const brandName = form.brandName.value;
+        const insertBrandName = brandName.toUpperCase();
+        const productType = form.productType.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const rating = form.rating.value;
+
+        const storeProductData = {
+            photo, name, insertBrandName, productType, price, description, rating
+        }
+
+
+
+        fetch("http://localhost:200/productData", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(storeProductData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    Swal.fire({
+                        title: 'success!',
+                        text: 'Successfully added a product !',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    });
+                    form.reset();
+                }
+            });
+    }
+
     return (
         <div className="w-[100%] bg-[#E7E7E7] py-24">
             <div className="w-[90%] md:w-[80%] xl:w-[70%] mx-auto">
-                <h2 className="text-4xl font-bold pb-8 text-center text-gray-500">Add Product</h2>
-                <form className="w-[90%] md:w-[80%] xl:w-[70%] mx-auto">
+                <h2 className="text-5xl font-bold pb-8 text-center text-gray-500">Add Product</h2>
+                <form onSubmit={handleAddProduct} className="w-[90%] md:w-[80%] xl:w-[70%] mx-auto">
                     <small className="m-2 text-gray-400">Product Image URL*</small>
                     <input className="w-[100%] h-12 bg-white outline-none rounded-md px-2" type="text" name="photo" placeholder="Image URL" required />
                     <br />
                     <br />
-                    <small className="m-2 text-gray-400">Name*</small>
+                    <small className="m-2 text-gray-400">Product Name*</small>
                     <input className="w-[100%] h-12 bg-white outline-none rounded-md  px-2" type="text" name="name" placeholder="Name" required />
                     <br />
                     <br />
                     <small className="m-2 text-gray-400">Brand Name*</small>
-                    <input className="w-[100%] h-12 bg-white outline-none rounded-md  px-2" type="email" name="brandName" required placeholder="Brand Name" />
+                    <input className="w-[100%] h-12 bg-white outline-none rounded-md  px-2" type="text" name="brandName" required placeholder="Brand Name" />
                     <br />
                     <br />
                     <small className="m-2 text-gray-400">Product Type*</small>
                     <select className="w-[100%] text-gray-500 h-12 bg-white outline-none rounded-md px-2" name="productType" required>
-                        <option>Select Product Type</option>
                         <option value="Mobile">Mobile</option>
                         <option value="Computer">Computer</option>
                         <option value="Head-Phone">Head-Phone</option>
